68e922fc98f5874907d1f5d77a37b17d6f7f9331
manisai-gupta <65009461+manisai-gupta@users.noreply.github.com>
2020-08-25 12:21:34 +0530

    programs

--- missing.py
+++ missing.py
@@ -1,12 +1,32 @@
-print("Enter the random  elements from 1 to 10  to find missing elemts from them...")
+# print("Enter the random  elements from 1 to 10  to find missing elemts from them...")
+# print("How many elements you want to enter:")
+# n=int(input())
+# s1={i for i in range(1,11)}
+# s2=set()
+# for i in range(n):
+#     number=int(input())
+#     s2.add(number)
+# print(s1.difference(s2))
+
+def missing(arr,n):
+    sum_n=((n+1)*(n+2))/2
+    # print(sum_n)
+    sum_user=0
+    for i in range(n):
+        sum_user+=arr[i]
+    print(sum_n-sum_user)
+    
 print("How many elements you want to enter:")
 n=int(input())
-s1={i for i in range(1,11)}
-s2=set()
+l=[]
 for i in range(n):
     number=int(input())
-    s2.add(number)
-print(s1.difference(s2))
+    l.append(number)
+n=len(l)
+# print(n)
+missing(l,n)
+
+
 
     
     
--- pairs.py
+++ pairs.py
@@ -1,10 +1,25 @@
-l=[1,2,3,4,5,6]
-n=len(l)
-k=6
-s=set()
-for i in range(n):
-    for j in range(0,n//2):
-        if k is i+j:
-            s.add((i,j))
-            print("The sum of target elements are ({},{})".format(i,j))
-print(s)
+# l=[1,2,3,4,5,6]
+# n=len(l)
+# k=6
+# s=set()
+# for i in range(n):
+#     for j in range(0,n//2):
+#         if k is i+j:
+#             s.add((i,j))
+#             print("The sum of target elements are ({},{})".format(i,j))
+# print(s)
+l1=[-1,1,5,7,2,3]
+n=len(l1)
+print("Enter the target element:")
+k=int(input())
+l2=[]
+for i in range(0,n):
+    a=l1.pop()
+    b=k-a
+    if b in l1:
+        l2.append((a,b))
+print(l2)
+
+
+
+    
