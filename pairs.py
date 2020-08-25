5af5ed65404b6fe5b8888037a1166aaf7b8595a7
manisai-gupta <65009461+manisai-gupta@users.noreply.github.com>
2020-08-25 22:16:54 +0530

    Update pairs.py

--- pairs.py
+++ pairs.py
@@ -8,17 +8,37 @@
 #             s.add((i,j))
 #             print("The sum of target elements are ({},{})".format(i,j))
 # print(s)
+
+
+# l1=[-1,1,5,7,2,3]
+# n=len(l1)
+# print("Enter the target element:")
+# k=int(input())
+# l2=[]
+# for i in range(0,n):
+#     a=l1.pop()
+#     b=k-a
+#     if b in l1:
+#         l2.append((a,b))
+# print(l2)
+
+
 l1=[-1,1,5,7,2,3]
 n=len(l1)
 print("Enter the target element:")
-k=int(input())
-l2=[]
-for i in range(0,n):
-    a=l1.pop()
-    b=k-a
-    if b in l1:
-        l2.append((a,b))
-print(l2)
+target=int(input())
+d=dict()
+for i, num in enumerate(l1):
+    n = target - num
+    if n not in d:
+        d[num] = i
+    else:
+        print([n, num])
+
+
+
+
+
 
 
 
