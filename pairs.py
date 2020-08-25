ac27507c51df2e9dd94b0a86ae8c019d07afc0ee
manisai-gupta <65009461+manisai-gupta@users.noreply.github.com>
2020-08-25 22:19:36 +0530

    Update pairs.py

--- pairs.py
+++ pairs.py
@@ -1,13 +1,4 @@
-# l=[1,2,3,4,5,6]
-# n=len(l)
-# k=6
-# s=set()
-# for i in range(n):
-#     for j in range(0,n//2):
-#         if k is i+j:
-#             s.add((i,j))
-#             print("The sum of target elements are ({},{})".format(i,j))
-# print(s)
+
 
 
 # l1=[-1,1,5,7,2,3]
